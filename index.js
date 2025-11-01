b787722832a260d84260f43635ebfbaba93ca313
eppa vishnu vardhan reddy <eppavishnuvardhanreddy@Hari-Shankars-MacBookAir.local>
2025-11-01 16:29:04 +0530

    added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const StoreInfo = require('./models/StoreInfo'); // Import our updated model
+const StoreInfo = require('./models/storeinfo'); // Import our updated model
 
 const app = express();
 const PORT = process.env.PORT || 3001;
